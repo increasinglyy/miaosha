83711f81ddcb47359ac8833d572e09c4d92b1ea0
increasinglyy <969989647@qq.com>
2019-04-24 20:01:46 +0800

    3-6~9 opt验证码获取，getotp页面

--- src/main/java/com/miaoshaproject/miaosha/controller/UserController.java
+++ src/main/java/com/miaoshaproject/miaosha/controller/UserController.java
@@ -11,12 +11,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 @Controller("user")
 @RequestMapping("/user")
+@CrossOrigin(allowCredentials = "true", allowedHeaders = "*")
 public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+
+    //接入HttpSession,httpServletRequest通过bean的方式注入
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户获取otp短信接口
+    @RequestMapping(value = "getotp", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone")String telphone){
+        //需要按照一定的规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);//此时随机数取值[0,99999)
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+
+        //将otp验证码同对应用户的手机号关联，
+        //使用HTTP session的方式绑定手机号与otpCode(redis非常适用）
+        httpServletRequest.getSession().setAttribute(telphone, otpCode);
+
+
+        //将otp验证码通过短信通道发送给用户，省略
+        System.out.println("telphone = "+telphone+" & optCode = " + otpCode);
+        return CommonReturnType.create(null);
+    }
+
 
     @RequestMapping("/get")
     @ResponseBody
