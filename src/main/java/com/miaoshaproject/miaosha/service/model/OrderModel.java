020ebabf06c69fd32bcca004527ab4f97663e92e
increasinglyy <969989647@qq.com>
2019-04-28 16:21:19 +0800

    5-1 交易模型创建

---
+++ src/main/java/com/miaoshaproject/miaosha/service/model/OrderModel.java
@@ -0,0 +1,73 @@
+package com.miaoshaproject.miaosha.service.model;
+
+import java.math.BigDecimal;
+
+//用户下单的交易模型
+public class OrderModel {
+
+    //订单号有一定的属性 201904280001
+    private String id;
+
+    //购买用户id
+    private Integer userId;
+
+    //购买商品id
+    private Integer itemId;
+
+    //购买商品的单价(下单时的价格）
+    private BigDecimal itemPrice;
+
+    //购买件数
+    private Integer amount;
+
+    //购买总金额
+    private BigDecimal orderPrice;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(BigDecimal orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+}
